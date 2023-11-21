8cf63d82927aca707f2dcf929f1ebbbcd8eae1d2
Li Ming Hsu <valentina.hsu@gmail.com>
2023-11-21 00:34:55 -0300

    faltan algunas funciones todavia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
 import Pedidos from "../components/Pedidos";
 import DropDownRestaurante from "../components/DropDownRestaurante";
+import axios from "axios";
 
-type TipoPedido= {
+type TipoPedido = {
     pedidos: string[];
     hora: string;
     id: number;
 }
-const handleDragEnterBasura=()=>{}
-  
-const PantallaRestaurante:React.FC = () => {
+
+export interface MenuTypes {
+    id: number;
+    category: string;
+    sideDish: boolean;
+    price: string;
+    name: string;
+    description: string;
+    image: string;
+}
+
+const handleDragEnterBasura = () => { }
+
+//aca esta la funcionnnn
+const getOrdersByCommand = async (commandId: number, field?: string) => {
+    try {
+        const response = await axios.get(`https://perfect-teal-beetle.cyclic.cloud/orderByCustomer/${commandId}`);
+        if (response.status === 200) {
+            const item = response.data;
+            if (field) {
+                const fieldValue = item[field];
+                return fieldValue;
+            } else {
+                return item;
+            }
+        } else { console.log("Item from Order not found"); }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const PantallaRestaurante: React.FC = () => {
     const [pedidosPendientes, setPedidosPendiente] = useState<TipoPedido[]>([
         { pedidos: ['Empanada', 'Churrasco'], hora: '12:15', id: 1 },
         { pedidos: ["Empanada", "Churrasco", "Provoleta", "a", "Volcan de chocolate con helado"], hora: '12:20', id: 2 },
@@ -35,121 +65,121 @@ const PantallaRestaurante:React.FC = () => {
         { pedidos: ["a", "b"], hora: '2:30', id: 24 },
 
     ]);
-    const[pendientes, setPendientes] = useState(true);
-    const[enProceso, setEnProceso] = useState(true);
-    const[completados, setCompletados] = useState(true);
-    const[moverse, setMoverse] = useState(true);
-    
+    const [pendientes, setPendientes] = useState(true);
+    const [enProceso, setEnProceso] = useState(true);
+    const [completados, setCompletados] = useState(true);
+    const [moverse, setMoverse] = useState(true);
+
     return <main className="no-scrollbar">
-    <div className="w-full min-h-screen h-full bg-white no-scrollbar">
-        <header className="h-fit w-full flex shadow-lg items-center">
-            <div className="pl-8 flex items-center h-full w-fit py-[10px] xl:py-5 ">
-                <img src="/TresLineas.svg" className="w-16 h-16" />
-                <button className="px-[84px]">
-                    <h3 className="text-black px-7 py-4 rounded-lg hover:bg-LineaPedido active:bg-ActiveHeaderRestaurante">Comandas</h3>
-                </button>
-                <button className="pr-[84px] ">
-                    <h3 className="text-black px-7 py-4 rounded-lg hover:bg-LineaPedido active:bg-ActiveHeaderRestaurante">Resumen</h3>
-                </button>
-                <button className="">
-                    <h3 className="text-black px-7 py-4 rounded-lg hover:bg-LineaPedido active:bg-ActiveHeaderRestaurante">Pedidos</h3>
-                </button>
-            </div>
-            <img src="/Logo.svg" className="h-[72px] w-24 absolute right-0 mr-8">
-
-            </img>
-        </header>
-        <DropDownRestaurante setPendientes={setPendientes} setCompletados={setCompletados} setEnProceso={setEnProceso} setMoverse={setMoverse}/>
-                
-        <div className="grid grid-cols-3 justify-around m-auto monitor:pt-6 xl:pt-11 z-10 ">
-            <div className="ml-20">
-            {pendientes ?(
-                <div className="custombp:h-[660px] notebook:h-[570px]  border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
-                    <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Pendientes</h4>
-                    {pedidosPendientes.map((pedidosMap, key)=>(
-                            <div key={key}>
-                                <Pedidos 
-                                setPedidoActual={setPedidosPendiente}
-                                pedidoEntero={pedidosPendientes} 
-                                pedidoActual={pedidosMap} 
-                                pedidos={pedidosMap.pedidos} 
-                                id={pedidosMap.id} 
-                                hora={pedidosMap.hora}
-                                color={"rojo"} 
-                                key={key}
-                                setPedidoDerecha={setpedidosEnproceso}
-                                setPedidoIzquierda={setpedidosCompletados}
-                                PedidoDerecha = {true}
-                                PedidoIzquierda = {false}
-                                moverse={moverse}
-                                ></Pedidos>
-                            </div>
-                    ))}
+        <div className="w-full min-h-screen h-full bg-white no-scrollbar">
+            <header className="h-fit w-full flex shadow-lg items-center">
+                <div className="pl-8 flex items-center h-full w-fit py-[10px] xl:py-5 ">
+                    <img src="/TresLineas.svg" className="w-16 h-16" />
+                    <button className="px-[84px]">
+                        <h3 className="text-black px-7 py-4 rounded-lg hover:bg-LineaPedido active:bg-ActiveHeaderRestaurante">Comandas</h3>
+                    </button>
+                    <button className="pr-[84px] ">
+                        <h3 className="text-black px-7 py-4 rounded-lg hover:bg-LineaPedido active:bg-ActiveHeaderRestaurante">Resumen</h3>
+                    </button>
+                    <button className="">
+                        <h3 className="text-black px-7 py-4 rounded-lg hover:bg-LineaPedido active:bg-ActiveHeaderRestaurante">Pedidos</h3>
+                    </button>
                 </div>
-            ):(<></>)}
+                <img src="/Logo.svg" className="h-[72px] w-24 absolute right-0 mr-8">
 
-            </div>
-            <div className="ml-6 mr-6 ">
-                {enProceso ?(
-                    <div className="custombp:h-[660px] notebook:h-[570px]  border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
-                        <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">En Proceso</h4>
-                        {pedidosEnproceso.map((pedidosMap, key)=>(
+                </img>
+            </header>
+            <DropDownRestaurante setPendientes={setPendientes} setCompletados={setCompletados} setEnProceso={setEnProceso} setMoverse={setMoverse} />
+
+            <div className="grid grid-cols-3 justify-around m-auto monitor:pt-6 xl:pt-11 z-10 ">
+                <div className="ml-20">
+                    {pendientes ? (
+                        <div className="custombp:h-[660px] notebook:h-[570px]  border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
+                            <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Pendientes</h4>
+                            {pedidosPendientes.map((pedidosMap, key) => (
                                 <div key={key}>
-                                    <Pedidos setPedidoActual={setpedidosEnproceso} 
-                                    pedidoActual={pedidosMap}
-                                    pedidoEntero={pedidosEnproceso}
-                                    pedidos={pedidosMap.pedidos} 
-                                    id={pedidosMap.id} 
-                                    hora={pedidosMap.hora} 
-                                    color={"amarillo"} 
-                                    key={key}
-                                    setPedidoDerecha={setpedidosCompletados}
-                                    setPedidoIzquierda={setPedidosPendiente}
-                                    PedidoDerecha = {true}
-                                    PedidoIzquierda = {true}
-                                    moverse={moverse}
+                                    <Pedidos
+                                        setPedidoActual={setPedidosPendiente}
+                                        pedidoEntero={pedidosPendientes}
+                                        pedidoActual={pedidosMap}
+                                        pedidos={pedidosMap.pedidos}
+                                        id={pedidosMap.id}
+                                        hora={pedidosMap.hora}
+                                        color={"rojo"}
+                                        key={key}
+                                        setPedidoDerecha={setpedidosEnproceso}
+                                        setPedidoIzquierda={setpedidosCompletados}
+                                        PedidoDerecha={true}
+                                        PedidoIzquierda={false}
+                                        moverse={moverse}
                                     ></Pedidos>
                                 </div>
-                        ))}
-                    </div>
-                ):( <></>)}
-                
-            </div>
-            <div className="mr-20">
-            {completados ?(
-                <div className="custombp:h-[660px] notebook:h-[570px]  border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
-                    <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Completados</h4>
-                    {pedidosCompletados.map((pedidosMap, key)=>(
-                            <div key={key}>
-                                <Pedidos setPedidoActual={setpedidosCompletados}
-                                    pedidoEntero={pedidosCompletados}
-                                    pedidoActual={pedidosMap}
-                                    pedidos={pedidosMap.pedidos} 
-                                    id={pedidosMap.id} 
-                                    hora={pedidosMap.hora} 
-                                    color={"verde"} 
-                                    key={key}
-                                    setPedidoDerecha={setPedidosPendiente}
-                                    setPedidoIzquierda={setpedidosEnproceso}
-                                    PedidoDerecha = {false}
-                                    PedidoIzquierda = {true}
-                                    moverse={moverse}
-                                    ></Pedidos>   
-                            </div>    
-                    ))}
+                            ))}
+                        </div>
+                    ) : (<></>)}
+
                 </div>
-            ):( <></> )}
+                <div className="ml-6 mr-6 ">
+                    {enProceso ? (
+                        <div className="custombp:h-[660px] notebook:h-[570px]  border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
+                            <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">En Proceso</h4>
+                            {pedidosEnproceso.map((pedidosMap, key) => (
+                                <div key={key}>
+                                    <Pedidos setPedidoActual={setpedidosEnproceso}
+                                        pedidoActual={pedidosMap}
+                                        pedidoEntero={pedidosEnproceso}
+                                        pedidos={pedidosMap.pedidos}
+                                        id={pedidosMap.id}
+                                        hora={pedidosMap.hora}
+                                        color={"amarillo"}
+                                        key={key}
+                                        setPedidoDerecha={setpedidosCompletados}
+                                        setPedidoIzquierda={setPedidosPendiente}
+                                        PedidoDerecha={true}
+                                        PedidoIzquierda={true}
+                                        moverse={moverse}
+                                    ></Pedidos>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (<></>)}
+
+                </div>
+                <div className="mr-20">
+                    {completados ? (
+                        <div className="custombp:h-[660px] notebook:h-[570px]  border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
+                            <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Completados</h4>
+                            {pedidosCompletados.map((pedidosMap, key) => (
+                                <div key={key}>
+                                    <Pedidos setPedidoActual={setpedidosCompletados}
+                                        pedidoEntero={pedidosCompletados}
+                                        pedidoActual={pedidosMap}
+                                        pedidos={pedidosMap.pedidos}
+                                        id={pedidosMap.id}
+                                        hora={pedidosMap.hora}
+                                        color={"verde"}
+                                        key={key}
+                                        setPedidoDerecha={setPedidosPendiente}
+                                        setPedidoIzquierda={setpedidosEnproceso}
+                                        PedidoDerecha={false}
+                                        PedidoIzquierda={true}
+                                        moverse={moverse}
+                                    ></Pedidos>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (<></>)}
+                </div>
+            </div>
+
+            <footer className="absolute bottom-0 w-full h-fit flex justify-center items-center pb-3 pt-5" >
+                <button>
+                </button>
+            </footer>
         </div>
-    </div>
-        
-        <footer className="absolute bottom-0 w-full h-fit flex justify-center items-center pb-3 pt-5" >
-            <button>
-            </button>
-        </footer>
-    </div>
-    
-</main>;
-    
+
+    </main>;
+
 }
 
 export default PantallaRestaurante
